0a9ef53003b6a11d92bb864a05281b62cb69cbbd
Sam <sam.dealy@gmail.com>
2025-02-16 15:35:34 -0500

    Use body-parser

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.use(express.json());
 app.post('/webhook', async (req, res) => {
     const payload = req.body;
     
