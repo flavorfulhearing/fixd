56b0c0728119d9ff85669220375a52845680bf05
flavorfulhearing <123139403+flavorfulhearing@users.noreply.github.com>
2025-02-18 22:10:02 -0500

    Auto-generated changes for: Remove all TODO comments

--- file-fetcher.js
+++ file-fetcher.js
@@ -1,3 +1,4 @@
+[file:file-fetcher.js]
 import { Octokit } from 'octokit';
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
@@ -7,8 +8,6 @@ export async function getRepositoryFiles(owner, repo) {
     const response = await octokit.rest.repos.getContent({ owner, repo, path: "" });
 
     for (const file of response.data) {
-        // TODO(samdealy): Figure out a better way to filter files. I.e. provide the minium number of relevant files to fetch.
-        // Potentially use embeddings to find the most relevant files.
         if (file.type === "file") {  
             const fileContent = await octokit.rest.repos.getContent({
                 owner,
