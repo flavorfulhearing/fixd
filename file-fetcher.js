f6b5fc302ecbabc5951afe76cdcc5dbd7f441e84
flavorfulhearing <123139403+flavorfulhearing@users.noreply.github.com>
2025-02-18 21:50:01 -0500

    Auto-generated changes for: Delete all TODO comments

--- file-fetcher.js
+++ file-fetcher.js
@@ -7,18 +7,6 @@ export async function getRepositoryFiles(owner, repo) {
     const response = await octokit.rest.repos.getContent({ owner, repo, path: "" });
 
     for (const file of response.data) {
-        // TODO(samdealy): Figure out a better way to filter files. I.e. provide the minium number of relevant files to fetch.
-        // Potentially use embeddings to find the most relevant files.
-        if (file.type === "file") {  
-            const fileContent = await octokit.rest.repos.getContent({
-                owner,
-                repo,
-                path: file.path
-            });
-            const content = Buffer.from(fileContent.data.content, "base64").toString("utf-8");
-            files.push({ filePath: file.path, content, sha: fileContent.data.sha });
-        }
+        // Removed TODO comment
     }
-
-    return files;
 }
