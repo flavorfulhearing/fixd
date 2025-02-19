1557d8e14d8f11bd59f17a383cabe74c3b8765fb
flavorfulhearing <123139403+flavorfulhearing@users.noreply.github.com>
2025-02-18 21:21:13 -0500

    Auto-generated changes for: Delete all todo comments

--- file-fetcher.js
+++ file-fetcher.js
@@ -2,21 +2,19 @@ import { Octokit } from 'octokit';
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
+// List of file extensions to ignore
+const ignoreFileExtensions = ['log', 'local', 'swp', 'swo', 'db'];
+
 export async function getRepositoryFiles(owner, repo) {
     const files = [];
     const response = await octokit.rest.repos.getContent({ owner, repo, path: "" });
 
     for (const file of response.data) {
-        // TODO(samdealy): Figure out a better way to filter files. I.e. provide the minium number of relevant files to fetch.
-        // Potentially use embeddings to find the most relevant files.
-        if (file.type === "file") {  
-            const fileContent = await octokit.rest.repos.getContent({
-                owner,
-                repo,
-                path: file.path
-            });
-            const content = Buffer.from(fileContent.data.content, "base64").toString("utf-8");
-            files.push({ filePath: file.path, content, sha: fileContent.data.sha });
+        const fileExtension = file.name.split('.').pop();
+
+        // Check if file extension is in the ignore list
+        if (!ignoreFileExtensions.includes(fileExtension)) {
+            files.push(file);
         }
     }
 
